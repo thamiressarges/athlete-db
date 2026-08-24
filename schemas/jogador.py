779e2842b870b03861ad098bb5ba183f7671d159
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-08-24 19:52:49 -0400

    feat: adiciona serialização de jogadores do MongoDB

---
+++ schemas/jogador.py
@@ -0,0 +1,13 @@
+def jogaodrEntidade(db_item) -> dict:
+    return {
+        'id': str(db_item['_id']),
+        'nome': db_item['nome'],
+        'idade': db_item['idade'],
+        'time': db_item['time']
+    }
+
+def listaJogadoresEntidade(db_item_lista) -> list:
+    lista_jogadores = []
+    for item in db_item_lista:
+        lista_jogadores.append(jogaodrEntidade(item))
+    return lista_jogadores
