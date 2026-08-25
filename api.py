48624ac82dc72a3cd147fe3476446987d109856b
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-08-25 13:59:25 -0400

    feat: configura CORS para permitir acesso do frontend

--- api.py
+++ api.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from routes.jogador import router
+from fastapi.middleware.cors import CORSMiddleware
+
+cliente_app = [
+    "http://localhost:3000"
+]
 
 app = FastAPI()
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cliente_app,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
