16dca22da8a705c264d552179e0ac9e1853ec5ff
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-08-25 13:30:25 -0400

    Atualiza

--- routes/jogador.py
+++ routes/jogador.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from config.database import connection
 from models.jogador import Jogador
+from schemas.jogador import jogaodrEntidade, listaJogadoresEntidade
 
 router = APIRouter()
 
@@ -10,4 +11,9 @@ async def inicio():
 
 @router.get('/jogadores')
 async def lista_jogadores():
-    return connection.local.jogador.find()
+    return listaJogadoresEntidade(connection.local.jogador.find())
+
+@router.post('/jogadores')
+async def cadastra_jogadores(jogador: Jogador):
+    connection.local.jogador.insert_one(dict(jogador))
+    return listaJogadoresEntidade(connection.local.jogadores.find())
